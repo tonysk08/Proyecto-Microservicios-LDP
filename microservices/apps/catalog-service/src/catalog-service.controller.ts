4dfb9dc0b26939b3e553f680bae961609687069d
tonysk08 <zarmiento227@gmail.com>
2026-06-10 04:07:14 +0000

    Refactor API Gateway and Catalog Service: Implement RabbitMQ integration and remove unused components

--- microservices/apps/catalog-service/src/catalog-service.controller.ts
+++ microservices/apps/catalog-service/src/catalog-service.controller.ts
@@ -10,8 +10,16 @@ export class CatalogServiceController {
     
     // Lo que retornes aquí se enviará automáticamente de vuelta al Gateway
     return [
-      { id: 1, name: 'Laptop Pro', price: 1200 },
-      { id: 2, name: 'Mouse Gamer', price: 50 },
+      { id: 1, name: 'Laptop Pro',        price: 1200 },
+      { id: 2, name: 'Mouse Gamer',       price: 50 },
+      { id: 3, name: 'Teclado Mecánico',  price: 100 },
+      { id: 4, name: 'Monitor 4K',        price: 300 },
+      { id: 5, name: 'Tablet',            price: 200 },
+      { id: 6, name: 'Smartphone',        price: 500 },
+      { id: 7, name: 'CPU',               price: 500 },
+      { id: 8, name: 'UPS',               price: 500 },
+      { id: 9, name: 'CPU FAN',           price: 500 },
+      { id: 9, name: 'Microfono',         price: 500 },
+      { id: 10,name: 'Altavoz',           price: 500 },
     ];
-  }
-}
+  }}
